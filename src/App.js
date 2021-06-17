d716493c29ba621828eebca34c9d129b71b44a7e
Karsenh <KarsenHansen@gmail.com>
2021-06-17 12:00:28 -0700

    Added basic css and axios get request for giphy data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Random } from './components/Random_V1';
+import { Tag_V1 } from './components/Tag_V1';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Random GIF Application</h1>
+      <div className='main-container'>
+        <Random />
+        <Tag_V1 />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Tag_V1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Tag_V1 = () => {
+    return (
+        <>
+            <h1>Tag</h1>
+        </>
+    )
+}
